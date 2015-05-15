3b5f8f41465e9269187fe3a486905156a1a32f0e
Dot Porter <dot.porter@gmail.com>
2015-05-15 14:06:59 -0400

    Deleted old data and adding new .sh files

---
+++ data/collation_formula.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+XSLTS="
+xslts/generate-formula.xsl"
+
+SUPPORT=`dirname $0`/support
+# collation-to-html.xsl
+
+index=0
+tmp=${TMPDIR:-/tmp}/prog.$$
+infile=$1
+outfolder=`echo $infile | awk -F '_' '{ print $1 }'`
+if [ -d $outfolder ]; then
+rm -rf $outfolder
+echo "Deleted existing folder $outfolder"
+fi
+for xsl in $XSLTS
+do
+	outfile=${tmp}_${index}
+	/Users/dorp/bin/saxon -s:"$infile" -xsl:$xsl -o:$outfile
+	echo "Wrote $outfile"
+	infile=$outfile
+	index=$(( $index + 1 ))
+done
+
+saxon -s:$infile -xsl:xslts/generate-formula.xsl
+
+
+
